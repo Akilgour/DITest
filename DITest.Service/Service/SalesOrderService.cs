074d14e248c388fd0c179ed3882c3db0121a429f
ashley <kilgourkilgour@hotmail.com>
2016-11-07 00:54:16 +0000

    Create sale order service

---
+++ DITest.Service/Service/SalesOrderService.cs
@@ -0,0 +1,21 @@
+﻿using DITest.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DITest.Service.Service
+{
+    public class SalesOrderService
+    {
+        public IEnumerable<SaleOrderDTO> GetAllSaleOrder()
+        {
+            var list = new List<SaleOrderDTO>();
+            list.Add(new SaleOrderDTO() { SaleOrderId = 1, AddressLineOne = "A", AddressLineTwo = "B" });
+            list.Add(new SaleOrderDTO() { SaleOrderId = 2, AddressLineOne = "C", AddressLineTwo = "D" });
+            list.Add(new SaleOrderDTO() { SaleOrderId = 3, AddressLineOne = "D", AddressLineTwo = "E" });
+
+            return list;
+        }
+    }
+}
