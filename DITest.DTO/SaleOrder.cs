b9021e4ac587d94344a501acdbe03b574aca1867
ashley <kilgourkilgour@hotmail.com>
2016-11-07 00:40:49 +0000

    Create project

---
+++ DITest.DTO/SaleOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DITest.DTO
+{
+    public class SaleOrderDTO
+    {
+        public int SaleOrderId { get; set; }
+        public string FullName { get; set; }
+        public string AddressLineOne { get; set; }
+        public string AddressLineTwo { get; set; }
+
+    }
+}
