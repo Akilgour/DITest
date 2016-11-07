0276e011455edc6992631310debe1a1ef40ad57c
ashley <kilgourkilgour@hotmail.com>
2016-11-07 01:49:13 +0000

    Create

---
+++ DITest/Controllers/SaleOrderController.cs
@@ -0,0 +1,26 @@
+﻿using DITest.Service.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DITest.Controllers
+{
+    public class SaleOrderController : Controller
+    {
+        private readonly ISalesOrderService service;
+
+        public SaleOrderController(ISalesOrderService service)
+        {
+            this.service = service;
+        }
+
+        // GET: SaleOrder
+        public ActionResult Index()
+        {
+            var foo = service.GetAllSaleOrder();
+            return View();
+        }
+    }
+}
---
+++ DITest/Models/SalesOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DITest.Models
+{
+    public class SalesOrder
+    {
+        public int SaleOrderId { get; set; }
+        public string FullName { get; set; }
+        public string AddressLineOne { get; set; }
+        public string AddressLineTwo { get; set; }
+    }
+}
