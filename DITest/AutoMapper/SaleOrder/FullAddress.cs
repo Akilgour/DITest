c98d55630b38c19a64d47f5e5f9d25364c363304
ashley <kilgourkilgour@hotmail.com>
2016-11-09 01:31:46 +0000

    Added fullName to stub data

--- DITest/AutoMapper/SaleOrder/FullAddress.cs
+++ DITest/AutoMapper/SaleOrder/FullAddress.cs
@@ -3,13 +3,12 @@ using DITest.DTO;
 
 namespace DITest.AutoMapper.SaleOrder
 {
-   public static class FullAddress //: ValueResolver<SalesOrder, string>
+    public static class FullAddress //: ValueResolver<SalesOrder, string>
     {
         // protected override string ResolveCore(SalesOrder source)
-       public static string ResolveCore(SaleOrderDTO source)
-
+        public static string ResolveCore(SaleOrderDTO source)
         {
-            return "foo bar"; 
+            return $"{source.FullName } lives at {source.AddressLineOne} {source.AddressLineTwo}";
         }
     }
 }
