ffdbc32115e8a2c14ea8700a7872c4ad352048b3
ashley <kilgourkilgour@hotmail.com>
2016-11-09 02:54:44 +0000

    Created EF create for saleOrder and saleIOrderItem

---
+++ DITest.Repository/Context/DITestContext.cs
@@ -0,0 +1,16 @@
+﻿using DITest.DTO.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DITest.Repository.Context
+{
+    public class DITestContext : DbContext
+    {
+        public DbSet<SaleOrderDTO> SaleOrder { get; set; }
+        public DbSet<SaleOrderItemDTO> SaleOrderItem { get; set; }
+    }
+}
---
+++ DITest.Repository/Migrations/201611090224208_InitialCreate.cs
@@ -0,0 +1,45 @@
+namespace DITest.Repository.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.SaleOrder",
+                c => new
+                    {
+                        SaleOrderId = c.Int(nullable: false, identity: true),
+                        FullName = c.String(),
+                        AddressLineOne = c.String(),
+                        AddressLineTwo = c.String(),
+                    })
+                .PrimaryKey(t => t.SaleOrderId);
+            
+            CreateTable(
+                "dbo.SaleOrderItem",
+                c => new
+                    {
+                        SaleOrderItemId = c.Int(nullable: false, identity: true),
+                        SaleOrderId = c.Int(nullable: false),
+                        Name = c.String(),
+                        Cost = c.String(),
+                        Quantity = c.String(),
+                    })
+                .PrimaryKey(t => t.SaleOrderItemId)
+                .ForeignKey("dbo.SaleOrder", t => t.SaleOrderId, cascadeDelete: true)
+                .Index(t => t.SaleOrderId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.SaleOrderItem", "SaleOrderId", "dbo.SaleOrder");
+            DropIndex("dbo.SaleOrderItem", new[] { "SaleOrderId" });
+            DropTable("dbo.SaleOrderItem");
+            DropTable("dbo.SaleOrder");
+        }
+    }
+}
