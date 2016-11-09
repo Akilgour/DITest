b0136eac70e98e90bcbbf52f6cd703e14ca30d91
ashley <kilgourkilgour@hotmail.com>
2016-11-09 03:09:30 +0000

    Added Seed data for sale order

---
+++ DITest.Repository/Migrations/SeedData/SaleOrder.cs
@@ -0,0 +1,27 @@
+﻿using DITest.DTO.Models;
+using DITest.Repository.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DITest.Repository.Migrations.SeedData
+{
+ 
+    public static class SaleOrder
+    {
+        public static void Seed(DITestContext context)
+        {
+            context.SaleOrder.AddOrUpdate(
+               p => p.SaleOrderId,
+              new SaleOrderDTO { SaleOrderId = 1, FullName = "Mr A", AddressLineOne = "1 A Road", AddressLineTwo = "A Town"  },
+              new SaleOrderDTO { SaleOrderId = 2, FullName = "Mr B", AddressLineOne = "2 B Road", AddressLineTwo = "B Town" },
+              new SaleOrderDTO { SaleOrderId = 3, FullName = "Mr C", AddressLineOne = "3 C Road", AddressLineTwo = "C Town" },
+              new SaleOrderDTO { SaleOrderId = 4, FullName = "Mr D", AddressLineOne = "4 D Road", AddressLineTwo = "D Town" },
+              new SaleOrderDTO { SaleOrderId = 5, FullName = "Mr E", AddressLineOne = "5 E Road", AddressLineTwo = "E Town" }
+            );
+        }
+    }
+}
