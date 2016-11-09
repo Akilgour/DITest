b2f5625c45acb81337da6b9d162a5bba07473451
ashley <kilgourkilgour@hotmail.com>
2016-11-09 01:31:55 +0000

    x

--- DITtest.Service/Services/SalesOrderService.cs
+++ DITtest.Service/Services/SalesOrderService.cs
@@ -13,9 +13,9 @@ namespace DITest.Service.Services
         public IEnumerable<SaleOrderDTO> GetAllSaleOrder()
         {
             var list = new List<SaleOrderDTO>();
-            list.Add(new SaleOrderDTO() { SaleOrderId = 1, AddressLineOne = "A", AddressLineTwo = "B" });
-            list.Add(new SaleOrderDTO() { SaleOrderId = 2, AddressLineOne = "C", AddressLineTwo = "D" });
-            list.Add(new SaleOrderDTO() { SaleOrderId = 3, AddressLineOne = "D", AddressLineTwo = "E" });
+            list.Add(new SaleOrderDTO() { SaleOrderId = 1, FullName = "x1", AddressLineOne = "A", AddressLineTwo = "B" });
+            list.Add(new SaleOrderDTO() { SaleOrderId = 2, FullName = "y1", AddressLineOne = "C", AddressLineTwo = "D" });
+            list.Add(new SaleOrderDTO() { SaleOrderId = 3, FullName = "z1", AddressLineOne = "D", AddressLineTwo = "E" });
 
             return list;
 
