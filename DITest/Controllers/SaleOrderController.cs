1d6dccc2ddb8973ab788a2814330b3ed781fa28c
ashley <kilgourkilgour@hotmail.com>
2016-11-14 16:12:15 +0000

    Added submit on save action

--- DITest/Controllers/SaleOrderController.cs
+++ DITest/Controllers/SaleOrderController.cs
@@ -35,6 +35,12 @@ namespace DITest.Controllers
             return View(new SalesOrder());
         }
 
+        [HttpPost]
+        public ActionResult Create(SalesOrder salesOrder)
+        {
+            return RedirectToAction("Index");
+        }
+
         public ActionResult EditAddress(int saleOrderId)
         {
             return base.PartialView("_EditAddress", Mapper.Map<SaleOrderDTO, SalesOrder>(service.GetSaleOrderById(saleOrderId)));
