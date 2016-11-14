eeedd84f2163b68b7924e78de64768a98124a613
ashley <kilgourkilgour@hotmail.com>
2016-11-14 12:12:36 +0000

    Added more validation for show made view show all errors

--- DITest/Models/SalesOrder.cs
+++ DITest/Models/SalesOrder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,7 +11,12 @@ namespace DITest.Models
     {
         public int SaleOrderId { get; set; }
         public string FullName { get; set; }
+        [DisplayName("Line One")]
+        [Required(ErrorMessage = "Line One is required")]
         public string AddressLineOne { get; set; }
+        [DisplayName("Line Two")]
+        [Required(ErrorMessage = "Line Two is required")]
+        [StringLength(10, ErrorMessage = "Line Two cannot be longer than 10 characters.")]
         public string AddressLineTwo { get; set; }
         [DisplayName("Full Address")]
         public string FullAddress { get; set; }
