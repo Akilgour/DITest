ddf8435264e96442b954a3def020cd743533b42b
ashley <kilgourkilgour@hotmail.com>
2016-12-27 21:12:52 +0000

    Created GetDisplayNameAttribute and tests

---
+++ DITest.Test.Nunit/Helpers/GetDisplayNameAttributeTest.cs
@@ -0,0 +1,45 @@
+﻿using DITest.Helpers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DITest.Test.Nunit.Helpers
+{
+    [TestFixture]
+    public class GetDisplayNameAttributeTest
+    {
+        [TestCase("PropertyOne", "Property Name One")]
+        [TestCase("PropertyTwo", "Property Name Two")]
+        [TestCase("PropertyThree", "PropertyThree")]
+        public void GetValue(string name, object expected)
+        {
+            //Arrange
+            var testModel = new TestModel();
+
+            //Act
+            var value = GetDisplayNameAttribute.Value(testModel, name);
+
+            //Assert
+            Assert.AreEqual(expected, value);
+        }
+    }
+
+
+    public class TestModel
+    {
+        public TestModel()
+        {
+        }
+        [DisplayName("Property Name One")]
+        public int PropertyOne { get; set; }
+        [DisplayName("Property Name Two")]
+        public int PropertyTwo { get; set; }
+        public int PropertyThree { get; set; }
+
+    }
+
+}
---
+++ DITest/Helpers/GetDisplayNameAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace DITest.Helpers
+{
+    public static class GetDisplayNameAttribute
+    {
+        public static string Value(object aaa, string name)
+        {
+            var bbb = aaa.GetType();
+            MemberInfo property = bbb.GetProperty(name);
+
+            var attributes = property.GetCustomAttributes(typeof(DisplayNameAttribute), true);
+
+            var attribute = property.GetCustomAttributes(typeof(DisplayNameAttribute), true)
+                  .Cast<DisplayNameAttribute>().SingleOrDefault();
+
+            if (attribute == null)
+            {
+                return name;
+            }
+
+            string ccc = attribute.DisplayName;
+            return ccc;
+        }
+    }
+}
