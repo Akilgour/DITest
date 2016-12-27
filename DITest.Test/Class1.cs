d3f72bdb3b3bb38ef50854e6667021fd1fba5def
ashley <kilgourkilgour@hotmail.com>
2016-12-27 20:23:42 +0000

    Create test project

---
+++ DITest.Test/Class1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DITest.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestOne()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
