841f04defc53643f067d5430144ca268982208e1
ashley <kilgourkilgour@hotmail.com>
2016-12-28 23:12:08 +0000

    Added IsDefaultValue and tests

---
+++ DITest.Test.Nunit/Helpers/IsDefaultValueTest.cs
@@ -0,0 +1,36 @@
+﻿using DITest.Helpers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DITest.Test.Nunit.Helpers
+{
+    [TestFixture]
+    public class IsDefaultValueTest
+    {
+
+        [TestCase(0, true)]
+        [TestCase(1, false)]
+        [TestCase("A", false)]
+        [TestCase("", true)]
+        [TestCase(" ", true)]
+        [TestCase("   ", true)]
+        [TestCase("0", false)]
+        [TestCase("1", false)]
+        public void GetValue(object item, bool expected)
+        {
+            //Arrange
+            var testModel = new TestModel();
+
+            //Act
+            var value = IsDefaultValue.Value(item);
+
+            //Assert
+            Assert.AreEqual(expected, value);
+        }
+
+    }
+}
---
+++ DITest/Helpers/IsDefaultValue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DITest.Helpers
+{
+    public static class IsDefaultValue
+    {
+        public static bool Value(object value)
+        {
+            if(value is string)
+            {
+                var item = value as string;
+                return string.IsNullOrWhiteSpace(item);
+            }
+            if (value is int)
+            {
+                var item = value as int?;
+                return item == 0;
+            }
+            return false;
+
+        }
+    }
+}
