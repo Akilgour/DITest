5aa6181356b4f2ba70a65c8620d0bcf6809557a2
ashley <kilgourkilgour@hotmail.com>
2016-12-30 18:07:11 +0000

    Crerated LargeObject model

---
+++ DITest/Models/LargeObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DITest.Models
+{
+    public class LargeObject
+    {
+        public int LargeObjectId { get; set; }
+        public string PropertyOne { get; set; }
+        public string PropertyTwo { get; set; }
+        public string PropertyThree { get; set; }
+        public string PropertyFour { get; set; }
+        public string PropertyFive { get; set; }
+        public string PropertySix { get; set; }
+        public string PropertySeven { get; set; }
+        public int PropertyEight { get; set; }
+        public bool PropertyNine { get; set; }
+        public Guid PropertyTen { get; set; }
+    }
+}
