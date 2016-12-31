26830592a39613c49854fd65fa5e10db0c910ce8
ashley <kilgourkilgour@hotmail.com>
2016-12-31 01:08:36 +0000

    Create SecondHalfLargeObject

---
+++ DITest/ViewModel/SecondHalfLargeObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DITest.ViewModel
+{
+    public class SecondHalfLargeObject
+    {
+        public int LargeObjectId { get; set; }
+
+        public string PropertySix { get; set; }
+        public string PropertySeven { get; set; }
+        public int PropertyEight { get; set; }
+        public bool PropertyNine { get; set; }
+        public Guid PropertyTen { get; set; }
+    }
+}
