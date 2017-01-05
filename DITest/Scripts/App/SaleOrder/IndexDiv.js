4346cb5a8513f617486cf6298d1d6162419c8a7d
ashley <kilgourkilgour@hotmail.com>
2017-01-05 00:50:53 +0000

    Cretd div table for shared div table logic

--- DITest/Scripts/App/SaleOrder/IndexDiv.js
+++ DITest/Scripts/App/SaleOrder/IndexDiv.js
@@ -1,6 +1,6 @@
-﻿$(document).ready(function () {
-    for (var i = 0; i < $('.divTableRowBackgroundColour').length; i = i + 4) {
-        $('.divTableRowBackgroundColour').eq(i).addClass('divTableRowAltBackgroundColour')
-        $('.divTableRowBackgroundColour').eq(i + 1).addClass('divTableRowAltBackgroundColour')
-    }
-})
+﻿//$(document).ready(function () {
+//    for (var i = 0; i < $('.divTableRowBackgroundColour').length; i = i + 4) {
+//        $('.divTableRowBackgroundColour').eq(i).addClass('divTableRowAltBackgroundColour')
+//        $('.divTableRowBackgroundColour').eq(i + 1).addClass('divTableRowAltBackgroundColour')
+//    }
+//})
---
+++ DITest/Scripts/App/Shared/divTable.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    for (var i = 0; i < $('.divTableRowBackgroundColour').length; i = i + 4) {
+        $('.divTableRowBackgroundColour').eq(i).addClass('divTableRowAltBackgroundColour')
+        $('.divTableRowBackgroundColour').eq(i + 1).addClass('divTableRowAltBackgroundColour')
+    }
+})
