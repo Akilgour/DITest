d15286841e09904fb2146b8ee16c0a4631a24ffb
ashley <kilgourkilgour@hotmail.com>
2017-01-05 00:28:48 +0000

    Added divTableRowBackgroundColour

---
+++ DITest/Scripts/App/SaleOrder/IndexDiv.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+
+    var numItems = $('.divTableRowBackgroundColour').length
+    var i = 0;
+    while (i < numItems) {
+        $('.divTableRowBackgroundColour').eq(i).css('background-color', 'red')
+        $('.divTableRowBackgroundColour').eq(i + 1).css('background-color', 'blue')
+        i = i + 4
+    }
+})
