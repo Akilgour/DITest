fe95c5e1d9f3361501483f79d1b9d93f84ab489f
ashley <kilgourkilgour@hotmail.com>
2017-01-13 23:46:17 +0000

    Added logic for click, to desable everything but the cliekd item

--- DITest/Scripts/App/LargeObject/Edit.js
+++ DITest/Scripts/App/LargeObject/Edit.js
@@ -2,5 +2,16 @@
 
     $(document).on("click", "#checkBoxNine", function (e) {
         alert("checkBoxNine click");
+        //  alert(e.val());
+
+        var clickedElementName = this.name;
+        $('form :input').each(function (i, e) {
+            if (e.name != clickedElementName) {
+               // $(e).css('backgroundColor', 'red');
+                $(e).val("");
+                $(e).prop('disabled', true);
+            }
+        });
+        this.prop('disabled', false);
     })
 });
